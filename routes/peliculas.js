c6b08e56799a43ee83e71ffab2f160f8dc6d84fb
Jaime de Greiff <alegreiff@gmail.com>
2019-01-27 12:04:04 -0500

    mensaje

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -27,7 +27,7 @@ var checkIDExist = function (req, res, next) {
             next();
         } else {
             //console.log('Book not found');
-            res.status(400).json('Película no encontrada. PAILAS');
+            res.status(400).json('That film does not exists');
         }
     });
 };
