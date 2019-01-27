810cca825b24f13aad1523a468a921dbcc645959
Jaime de Greiff <alegreiff@gmail.com>
2019-01-27 18:50:32 -0500

    CORS added

--- routes/fechas.js
+++ routes/fechas.js
@@ -14,19 +14,19 @@ var checkIDInput = function (req, res, next) {
 };
 var checkIDExist = function (req, res, next) {
     //console.log('Check ID exist');
-    Pelicula.count({ where: { id: req.params.id } }).then(count => {
+    Fecha.count({ where: { id: req.params.id } }).then(count => {
         if (count != 0) {
             next();
         } else {
             //console.log('Book not found');
-            res.status(400).json('Book not found');
+            res.status(400).json('Fecha no hallada');
         }
     });
 };
 
 router.get('/', function(req, res){
-    Pelicula.findAll().then(peli => {
-        res.status(200).json(peli);
+    Fecha.findAll().then(fecha => {
+        res.status(200).json(fecha);
     });
 });
 
