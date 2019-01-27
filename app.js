764973975f2a4c9eff3c80ca2fd071eed9327394
Jaime de Greiff <alegreiff@gmail.com>
2019-01-27 13:24:11 -0500

    CORS added

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var cors = require('cors')
 var app = express();
-app.use(cors())
+app.use(cors({
+  "origin": ""
+}))
 var bodyParser = require('body-parser');
 var sqlite = require('sqlite3');
 var env = require('dotenv').load();
