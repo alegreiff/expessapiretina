c129aa6971c5d4e942e6ca8b8fe1567fc91690ef
Jaime de Greiff <alegreiff@gmail.com>
2019-01-27 12:11:46 -0500

    mensaje malo

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -27,7 +27,7 @@ var checkIDExist = function (req, res, next) {
             next();
         } else {
             //console.log('Book not found');
-            res.status(400).json('That film does not exists');
+            res.status(400).json('Puta vida. No está...');
         }
     });
 };
