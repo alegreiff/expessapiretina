0a8f6d30e9e7809046883d5b8895b6f5213cb321
Jaime de Greiff <alegreiff@gmail.com>
2019-01-27 13:15:07 -0500

    CORS added

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -27,7 +27,7 @@ var checkIDExist = function (req, res, next) {
             next();
         } else {
             //console.log('Book not found');
-            res.status(400).json('No existe película con dicho ID. ERROR');
+            res.status(400).json('JAJAJA No existe película con dicho ID. ERROR');
         }
     });
 };
