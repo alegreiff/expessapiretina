f3cbdb38eacb5e766c2b758dfb0ce873a39d384c
Jaime de Greiff <alegreiff@gmail.com>
2019-01-27 12:17:13 -0500

    okok

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -27,7 +27,7 @@ var checkIDExist = function (req, res, next) {
             next();
         } else {
             //console.log('Book not found');
-            res.status(400).json('Kokoroyó');
+            res.status(400).json('Esa puta película la conocerá su madre');
         }
     });
 };
