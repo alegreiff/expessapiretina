4b41da3c08b6c5d31f1f110742143155b03569ab
Jaime de Greiff <alegreiff@gmail.com>
2019-02-08 17:01:46 -0500

    Saved

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 //https://github.com/masfranzhuo/sequalize-express-SQLite
 const compression = require('compression');
 var express = require('express');
-var cors = require('cors')
 var app = express();
-
-
-app.use(cors())
 app.use(compression());
 
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
 
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});*/
+});
 
 var bodyParser = require('body-parser');
 var sqlite = require('sqlite3');
