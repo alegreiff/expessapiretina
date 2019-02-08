1610ea7638667365ca7eb96bedf2221f68f3f644
Jaime de Greiff <alegreiff@gmail.com>
2019-02-08 15:57:30 -0500

    sin auth CORS

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const compression = require('compression');
 var express = require('express');
 var cors = require('cors')
 var app = express();
-app.use(cors())
+
+var whitelist = ['http://localhost:3000', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.use(compression());
 
 
