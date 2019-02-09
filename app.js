fcffaab324df51d718c7db28f0101b5f0a9e4484
Jaime de Greiff <alegreiff@gmail.com>
2019-02-08 21:40:54 -0500

    saXX

--- app.js
+++ app.js
@@ -4,27 +4,8 @@ var express = require('express');
 var cors = require('cors')
 var app = express();
 
-var whitelist = ['*']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-const whitelist = ['http://localhost:3000', 'https://js.presencia.co']
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
+
+app.use(cors())
 app.use(compression());
 
 
