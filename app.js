4fd7c0e9f0e1147ca1bde2793fb5a7e8ccf6fb61
Jaime de Greiff <alegreiff@gmail.com>
2019-02-08 21:38:48 -0500

    saXX

--- app.js
+++ app.js
@@ -14,7 +14,17 @@ var corsOptions = {
     }
   }
 }
-app.use(cors())
+const whitelist = ['http://localhost:3000', 'https://js.presencia.co']
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.use(compression());
 
 
