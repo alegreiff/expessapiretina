104f58b4d8bdfc91578fe5c9f4a170cffb0d93df
Jaime de Greiff <alegreiff@gmail.com>
2019-02-08 21:45:50 -0500

    saXX

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -69,7 +69,7 @@ router.put('/:id', (req, res) => {
 const cors = require('cors')
 
 const corsOptions = {
-  origin: 'http://localhost:3006'
+  origin: 'http://localhost:3000'
 }
 router.post('/', cors(corsOptions), function(req, res){
   console.log(req)
