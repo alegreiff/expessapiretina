a44b7b6f838cd6442f58869451b85cd74eb2d981
Jaime de Greiff <alegreiff@gmail.com>
2019-03-04 09:26:18 -0500

    lastets

--- routes/estadisticas.js
+++ routes/estadisticas.js
@@ -1,27 +1,25 @@
-var express = require('express');
-var Estadistica = require('../models').Estadistica;
-var Edad = require('../models').Edad;
-var Adquisicion = require('../models').Adquisicion;
-var Genero = require('../models').Genero;
-var Tecnologia = require('../models').Tecnologia;
+var express = require("express");
+var Estadistica = require("../models").Estadistica;
+var Edad = require("../models").Edad;
+var Adquisicion = require("../models").Adquisicion;
+var Genero = require("../models").Genero;
+var Tecnologia = require("../models").Tecnologia;
 var router = express.Router();
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../firebase.json");
+//var serviceAccount = require("../firebase.json");
 
-const cors = require('cors')
+const cors = require("cors");
 
 const corsOptions = {
-  origin: ['http://localhost:3006', 'dev.inforetinalatina.org']
-}
+  origin: ["http://localhost:3006", "dev.inforetinalatina.org"]
+};
 
-
-admin.initializeApp({
+/* admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://estadisticasretina.firebaseio.com"
-});
-
+}); */
 
 /*
 admin.auth().verifyIdToken(idToken)
@@ -35,51 +33,68 @@ admin.auth().verifyIdToken(idToken)
 */
 
 /* req.headers.authorization */
-var checkAUTH = function (req, res, next) {
-  console.log(req.headers.authorization)
-  let token = req.headers.authorization.split(' ')[1];
-  admin.auth().verifyIdToken(token)
-  .then(function(decodedToken) {
-    //var uid = decodedToken.uid;
-    next()
-  }).catch(function(error) {
-    res.status(400).json('NO AUTH FOR OLD MEN');
-  });  
-  
+var checkAUTH = function(req, res, next) {
+  console.log(req.headers.authorization);
+  let token = req.headers.authorization.split(" ")[1];
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(function(decodedToken) {
+      //var uid = decodedToken.uid;
+      next();
+    })
+    .catch(function(error) {
+      res.status(400).json("NO AUTH FOR OLD MEN");
+    });
 };
 
-
 //router.get('/', [checkAUTH], function(req, res){
-router.get('/', cors(corsOptions), function(req, res){
-    Estadistica.findAll({
-      attributes: ['id', 'mes', 'year', 'sesiones', 'kaltura', 'usuarios_analytics','duracion_media','rebote','nuevas_sesiones','usuarios_wp','visitas_paginas'],
-      include: [{
-        model: Edad, as: 'edad',
-        attributes: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
-      },
-      {
-        model: Adquisicion, as: 'adquisicion',
-        attributes: ['directa', 'social', 'organica', 'referida']
-      },
-      {
-        model: Genero, as: 'genero',
-        attributes: ['hombres', 'mujeres']
-      },
-      {
-        model: Tecnologia, as: 'tecnologia',
-        attributes: ['escritorio', 'movil', 'tablet']
-      }
+router.get("/", cors(corsOptions), function(req, res) {
+  Estadistica.findAll({
+    attributes: [
+      "id",
+      "mes",
+      "year",
+      "sesiones",
+      "kaltura",
+      "usuarios_analytics",
+      "duracion_media",
+      "rebote",
+      "nuevas_sesiones",
+      "usuarios_wp",
+      "visitas_paginas"
     ],
+    include: [
+      {
+        model: Edad,
+        as: "edad",
+        attributes: ["g1", "g2", "g3", "g4", "g5", "g6"]
+      },
+      {
+        model: Adquisicion,
+        as: "adquisicion",
+        attributes: ["directa", "social", "organica", "referida"]
+      },
+      {
+        model: Genero,
+        as: "genero",
+        attributes: ["hombres", "mujeres"]
+      },
+      {
+        model: Tecnologia,
+        as: "tecnologia",
+        attributes: ["escritorio", "movil", "tablet"]
+      }
+    ]
   }).then(estadistica => {
-        res.status(200).json(estadistica);
-    });
+    res.status(200).json(estadistica);
+  });
 });
 
-
-
-router.post('/', function(req, res){
-  console.log(req)
-    Estadistica.create({
+router.post("/", function(req, res) {
+  console.log(req);
+  Estadistica.create(
+    {
       mes: req.body.mes,
       year: req.body.year,
       sesiones: req.body.sesiones,
@@ -95,54 +110,56 @@ router.post('/', function(req, res){
         organica: req.body.adquisicion[0].organica,
         referida: req.body.adquisicion[0].referida
       },
-      tecnologia:
-      {
+      tecnologia: {
         escritorio: req.body.tecnologia[0].escritorio,
         movil: req.body.tecnologia[0].movil,
         tablet: req.body.tecnologia[0].tablet
       },
-      genero:
-      {
+      genero: {
         hombres: req.body.genero[0].hombres,
         mujeres: req.body.genero[0].mujeres
       },
-      edad:
-      {
+      edad: {
         g1: req.body.edad[0].g1,
         g2: req.body.edad[0].g2,
         g3: req.body.edad[0].g3,
         g4: req.body.edad[0].g4,
         g5: req.body.edad[0].g5,
         g6: req.body.edad[0].g6
-
-      }},{include: [{
-        model: Edad,
-        as: 'edad'
-      },
-      {
-        model: Genero,
-        as: 'genero'
-      },
-      {
-        model: Adquisicion,
-        as: 'adquisicion'
-      },
-      {
-        model: Tecnologia,
-        as: 'tecnologia'
-      }]
-    }).then(estadistica => {
-        res.status(200).json(estadistica);
+      }
+    },
+    {
+      include: [
+        {
+          model: Edad,
+          as: "edad"
+        },
+        {
+          model: Genero,
+          as: "genero"
+        },
+        {
+          model: Adquisicion,
+          as: "adquisicion"
+        },
+        {
+          model: Tecnologia,
+          as: "tecnologia"
+        }
+      ]
+    }
+  )
+    .then(estadistica => {
+      res.status(200).json(estadistica);
     })
     .catch(error => {
-      console.log(error)
-      res.status(404).send(error.message)
+      console.log(error);
+      res.status(404).send(error.message);
     })
     .error(err => {
-        console.log(error)
-        res.status(405).json('Error has occured');
+      console.log(error);
+      res.status(405).json("Error has occured");
     });
 });
-
 
 module.exports = router;
